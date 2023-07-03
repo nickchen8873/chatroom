1e1ec292f515417fc7a3b87ce740e3a225cc8e7e
nickChen <jkzlv666889575@gmail.com>
2023-07-03 17:07:07 +0800

    first verison

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    
+      build: {
+        transpile: ["@heroicons/vue", "@headlessui/vue"],
+      },
+    
+      css: ["@/assets/css/main.css"],
+    
+    
+      // modules: ["~/modules/socket.ts"],
+})
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <Head>
+      <Title>Nuxt 3 Websocket</Title>
+    </Head>
+  
+    <!-- <Header /> -->
+  
+    <div class="h-screen w-full grid place-items-center bg-gray-200 dark:bg-black">
+      <span class="flex flex-col items-centers space-y-4">
+        <span class="text-slate-900 dark:text-gray-100">{{ message }}</span>
+        <button @click="sendMessage" class="bg-purple-500 text-gray-50 font-semibold px-5 py-2 rounded-lg">Click me to send random number</button>
+      </span>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { v4 as uuid } from "uuid"
+  const message = ref<string>("")
+  const { $socket } = useNuxtApp()
+   const uid = uuid()
+  onMounted(() => {
+     $socket.onopen = () => {
+      localStorage.setItem(`connection-${uid}`, uid)
+      $socket.send(uid)
+    }
+    $socket.onmessage = ({ data }: any) => {
+      console.log("data", data)
+      message.value = data
+    }
+    $socket.onclose = function () {
+      console.log("disconnected")
+    }
+  })
+  const sendMessage = () => {
+      fetch("/api/sendmessage", {
+        method: "POST",
+        body: JSON.stringify({ message: Math.random(), sender: localStorage.getItem(`connection-${uid}`) }),
+      }).then(res => res.json()).then(data =>{
+          console.log("sent")
+      })
+  }
+  </script>
