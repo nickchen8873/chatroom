126a8225fbd5f6597d169d5d81f56ad72237b01e
nickChen <jkzlv666889575@gmail.com>
2023-09-24 14:20:57 +0800

    finish demo version

---
+++ server/api/socketServer.js
@@ -0,0 +1,48 @@
+import { createServer } from 'https';
+import { Server } from 'socket.io';
+
+import { readFileSync } from 'fs';
+
+const privateKey = readFileSync('C:\\Users\\coolman\\Documents\\chatroom\\localhost-key.pem');
+const certificate = readFileSync('C:\\Users\\coolman\\Documents\\chatroom\\localhost.pem');
+
+const serverOptions = { key: privateKey, cert: certificate };
+
+// 創建HTTP Server
+const httpServer = createServer(serverOptions);
+
+// 創建Socket.io Server
+const io = new Server(httpServer, {
+  // origin: "https://localhost",
+  //   methods: ["GET", "POST"],
+  //   allowedHeaders: ["my-custom-header"],
+  //   credentials: true
+});
+
+let messages = []
+
+// 處理連接事件
+io.on('connection', (socket) => {
+  console.log('有新的連線');
+
+  // 發送之前的全部訊息
+  io.emit("allMessage", messages)
+
+  // 當此用戶發送訊息的時候，先把新訊息放到 messages 陣列裡面
+    // 再 emit 給所有用戶
+  socket.on('chat message', (message) => {
+    console.log('收到消息：', message);
+    messages.push(message)
+    io.emit('chat message', message);
+  });
+
+  // 處理斷開連線事件
+  socket.on('disconnect', () => {
+    console.log('連線斷開');
+  });
+});
+
+// 監聽port
+httpServer.listen(4000, () => {
+  console.log('Socket.io Server運行在https://localhost:4000');
+});
